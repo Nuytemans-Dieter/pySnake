42fbd15a2b91da0950087b12a041187619d172dc
Dieter Nuytemans <37397560+Nuytemans-Dieter@users.noreply.github.com>
2020-01-10 19:29:48 +0100

    Add score display

--- visualizer/printboard.py
+++ visualizer/printboard.py
@@ -3,8 +3,10 @@ import game
 class BoardPrinter:
     def drawBoard (self, board):
         game_space = board.get_game_view()
-        game_view = ""
 
+        game_view =  "|--------------|" + "\n"
+        game_view += "| Score: " + str(board.score) + (6 - str(board.score).__len__()) * " " + "|\n"
+        game_view += "|--------------|" + "\n"
         game_view += (game.Board.BOARD_DIM_X + 2) * "+ " + "\n"
 
         for row in game_space:
