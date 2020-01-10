c65270230f14884a118ffd05ddc6dfa2ed84d298
Dieter Nuytemans <37397560+Nuytemans-Dieter@users.noreply.github.com>
2020-01-10 21:14:42 +0100

    Improve better_agent

--- move_input/better_agent.py
+++ move_input/better_agent.py
@@ -19,8 +19,12 @@ class BetterAgent:
 
         # Get starting parameters
         (distance_to_dot, to_tail, to_wall) = board.get_game_data()
-        total_heuristic = distance_to_dot
-        new_move = None
+        possible_moves = {
+                            Moves.UP:    -1,
+                            Moves.RIGHT: -1,
+                            Moves.DOWN:  -1,
+                            Moves.LEFT:  -1
+                         }
         
         # Iterate through all possible moves
         for move in board.get_successors():
@@ -28,15 +32,21 @@ class BetterAgent:
             new_head_location = add_vectors_2D(head_location, board.DIRECTION_OFFSET_VECTOR[move])
             new_distance = distance_squared(new_head_location, board.dot_location)
             
-            # Handle game-losing moves
-            distance_to_tail = self.get_distance_to_tail(board, body_locations)
-            if distance_to_tail is -1:
-                new_heuristic = new_distance
-                if new_heuristic < total_heuristic:
-                    total_heuristic = new_heuristic
-                    new_move = move
+            # Handle game-losing moves and add each non-terminal move
+            if board.is_location_within_bounds(new_head_location):
+                if not body_locations.__contains__(new_head_location):
+                    possible_moves[move] = new_distance
 
-        return new_move
+        # Select the best move out of all possibilites
+        best_distance = -1
+        best_move = None
+        for move, distance in possible_moves.items():
+            if distance is not -1:
+                if distance < best_distance or best_distance is -1:
+                    best_distance = distance
+                    best_move = move
+
+        return best_move
 
 
     def calculate_heuristic(self, game_data):
